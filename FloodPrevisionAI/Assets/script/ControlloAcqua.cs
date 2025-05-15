5ba281302be4760f5f60a8e3c182b0a4e5b9f6e6
Marco Balducci <balduccimarco2007@gmail.com>
2025-05-15 09:48:27 +0200

    added saturation logic

--- FloodPrevisionAI/Assets/script/ControlloAcqua.cs
+++ FloodPrevisionAI/Assets/script/ControlloAcqua.cs
@@ -6,29 +6,60 @@ public class ControlloAcqua : MonoBehaviour
 {
     [SerializeField] float changeDelay = 3f;
     [SerializeField] float animDuration = 1f;
-    [SerializeField] float minHeightDry = 1f;
-    [SerializeField] float minHeightSaturated = 1f;
-    float minHeight;
-    [SerializeField] float maxHeightDry = 1f;
-    [SerializeField] float maxHeightSaturated = 1f;
-    float maxHeight;
+    [SerializeField] float MINHEIGHT;
+    [SerializeField] float MAXHEIGHT;
+
+    [Space]
     [SerializeField] AnimationCurve animCurve;
-    float animTimer;
+    [SerializeField] float animTimer;
     IEnumerator loop;
 
     [Space]
+    [SerializeField] public float terrainSaturation;
+    [SerializeField] float decrementSpeed = 2.5f;
+    [SerializeField] float minHeight;
+    [SerializeField] float maxHeight;
 
-    [SerializeField] float terrainSaturation;
-    
+    [Space]
+    [SerializeField] public float rainWeight;
+    [SerializeField] public float rainWeightPower;
+
+    const float terrainSaturationMax = 1f;
+    float lastSaturation;
+
     private void Start()
     {
+        lastSaturation = terrainSaturation;
+        minHeight = MINHEIGHT;
+        maxHeight = MAXHEIGHT;
+
         loop = WaterLoop();
         StartCoroutine(loop);
     }
 
     private void Update()
     {
+        // change height window based on saturation
+        if(terrainSaturation != lastSaturation)
+        {
+            minHeight += (terrainSaturation - lastSaturation) * MINHEIGHT * 5;
+            maxHeight += (terrainSaturation - lastSaturation) * MAXHEIGHT;
 
+            lastSaturation = terrainSaturation;
+        }
+
+        // consider rain impact on saturation
+        terrainSaturation = Mathf.Min(terrainSaturation + rainWeight * rainWeightPower * Time.deltaTime, terrainSaturationMax);
+
+        // remove saturation over time
+        if (terrainSaturation > 0)
+        {
+            terrainSaturation -= decrementSpeed * Time.deltaTime;
+            if (terrainSaturation < 0)
+            {
+                terrainSaturation = 0;
+            }
+        }
     }
 
     IEnumerator WaterLoop()
