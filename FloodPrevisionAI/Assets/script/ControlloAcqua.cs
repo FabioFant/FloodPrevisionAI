92db3a00930aaf893462392d7b36874caabcf5d6
FedeGothEnjoyer <federicopalmio@gmail.com>
2025-05-16 13:01:34 +0200

    aggiunto buffer di previsione tempo

--- FloodPrevisionAI/Assets/script/ControlloAcqua.cs
+++ FloodPrevisionAI/Assets/script/ControlloAcqua.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -23,6 +24,7 @@ public class ControlloAcqua : MonoBehaviour
 
     [Space]
     [SerializeField] public float rainWeight;
+    [Range(0.0f, 1.0f)]
     [SerializeField] public float rainWeightPower;
 
     const float terrainSaturationMax = 1f;
@@ -37,14 +39,27 @@ public class ControlloAcqua : MonoBehaviour
     [Header("UI")]
     [SerializeField] TMP_Text saturationPercent;
 
+    
+    [Space]
+    [Header("Weather Simulation")]
+    [SerializeField] float hourDuration = 1f;
+    [SerializeField] AnimationCurve rainCurve;
+    [SerializeField] List<float> rainBuffer;
+    [SerializeField] int bufferSize = 5;
+    [Range(0.0f, 1.0f)]
+    [SerializeField] float rainThreshold = 0.5f; //il valore sotto il quale il tempo è considerato senza pioggia
+
+
     private void Start()
     {
         lastSaturation = terrainSaturation;
         minHeight = MINHEIGHT;
         maxHeight = MAXHEIGHT;
 
-        loop = WaterLoop();
-        //StartCoroutine(loop);
+        loop = RainLoop();
+
+        FillBuffer();
+        StartCoroutine(loop);
     }
 
     private void Update()
@@ -76,10 +91,44 @@ public class ControlloAcqua : MonoBehaviour
             }
         }
 
+
+        //effetti grafici
         rainEffect.emissionRate = rainIncrement * rainWeightPower;
         saturationPercent.text = Mathf.Round(terrainSaturation * 100).ToString() + "%";
     }
 
+    IEnumerator RainLoop()
+    {
+        //loop che determina la variazione di potenza della pioggia nel tempo
+        while (true)
+        {
+            float timer = hourDuration;
+            while (timer > 0f)
+            {
+                timer-=Time.deltaTime;
+                //la potenza è interpolata tra i due valori nel buffer utilizzando la curva di animazione
+                rainWeightPower = rainBuffer[0] + (rainBuffer[1] - rainBuffer[0]) * rainCurve.Evaluate((hourDuration-timer)/hourDuration);
+                yield return null;
+            }
+            rainBuffer.RemoveAt(0);
+            if( rainBuffer.Count < bufferSize) FillBuffer();
+        }
+    }
+
+    void FillBuffer()
+    {
+        //riempie il buffer delle previsioni con piogge casuali
+
+        for (int i = 0; i < bufferSize; i++)
+        {
+            float res = Random.Range(0f, 1f); //rain weight power casuale tra 0 e 1
+            //il perlin noise non funzionava ma può essere riprovato per valori più coerenti
+            rainBuffer.Add(res > rainThreshold ? res : 0);
+        }
+    }
+
+
+    //inutilizzato
     IEnumerator WaterLoop()
     {
         while (true)
